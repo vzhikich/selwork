63161faff7a41bbdde5174d2f93f5184a236d70f
Yevhenii Flyk <yevhenii.flyk.work@gmail.com>
2024-03-24 11:09:14 +0200

    init commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDfvcdaO53QVUIVI94xyJiRmNggxDHpKhA",
+//     authDomain: "selfwork-dnd.firebaseapp.com",
+//     databaseURL: "https://selfwork-dnd-default-rtdb.firebaseio.com",
+//     projectId: "selfwork-dnd",
+//     storageBucket: "selfwork-dnd.appspot.com",
+//     messagingSenderId: "761999415379",
+//     appId: "1:761999415379:web:80f432eac33da0d69acea6"
+// }
+// 
+// firebase.initializeApp(firebaseConfig);
+
+// var contactFormDB = firebase.database().ref('selfworkDnD');
+
+// document.getElementById("registerForm").addEventListener("submit", submitLogin);
+
+
+
+const button = document.getElementById('CharactersBtn');
+const createButton = document.getElementById('CharactersCreateBtn');
+
+createButton.addEventListener('click', function(){
+    window.location.href = 'charactersCreate.html';
+})
+
+button.addEventListener('click', function() {
+    window.location.href = 'characters.html';
+});
