1930964fa11ac0a5946a69f4a1b959d9c82c4f66
Yevhenii Flyk <yevhenii.flyk.work@gmail.com>
2024-03-28 19:27:53 +0200

    characterCreate page add

--- js/charactersCreate.js
+++ js/charactersCreate.js
@@ -3,3 +3,15 @@ const BackButton = document.getElementById('backBtn');
 BackButton.addEventListener('click', function(){
     window.location.href = 'index.html';
 })
+
+function changeOptionRace(option) {
+    document.querySelector('.Race').textContent = option;
+}
+
+function changeOptionClass(option) {
+    document.querySelector('.ClassList').textContent = option;
+}
+
+function changeOptionHistory(option) {
+    document.querySelector('.HistoryCharac').textContent = option;
+}
